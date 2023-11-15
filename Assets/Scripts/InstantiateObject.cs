394f83f1e676826a601155be0d5ed30b324ff934
Tiago V <T.Verstoep96@gmail.com>
2023-11-15 11:42:34 +0100

    Revert "Merge branch 'main' into zoebranch"
    This reverts commit b3426057968de9ba6e1dbe7fe4f3d44b7d6f7489, reversing
    changes made to 7191f04d56c04a49b49e944f243c13c8ec7cbcc8.

--- Assets/Scripts/InstantiateObject.cs
+++ Assets/Scripts/InstantiateObject.cs
@@ -7,22 +7,17 @@ public class InstantiateObject : MonoBehaviour
     [SerializeField]
     private GameObject prefabToInstantiate2; // Reference to the prefab you want to instantiate
 
-    private Vector3 mousePos;
-
     private void Update()
     {
-        mousePos = Input.mousePosition;
-        Vector3 spawnPosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 15f));
-
         if (Input.GetKeyDown(KeyCode.Alpha1)) // Change 'Space' to the desired key
         {
             // Instantiate the prefab at a specific position and rotation
-            Instantiate(prefabToInstantiate1, spawnPosition, Quaternion.identity);
+            Instantiate(prefabToInstantiate1, new Vector3(0, 0, 15), Quaternion.identity);
         }
         if (Input.GetKeyDown(KeyCode.Alpha2)) // Change 'Space' to the desired key
         {
             // Instantiate the prefab at a specific position and rotation
-            Instantiate(prefabToInstantiate2, spawnPosition, Quaternion.identity);
+            Instantiate(prefabToInstantiate2, new Vector3(0, 0, 15), Quaternion.identity);
         }
     }
 }
