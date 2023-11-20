36583526988caff18a9c076e19b467650511988d
zoe horwitz <zoehorwitz@xs4all.nl>
2023-11-20 12:15:56 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ Assets/Scripts/JoystickCursorControl.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class JoystickCursorControl : MonoBehaviour
+{
+    public float cursorSpeed = 5.0f;
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput).normalized;
+
+        // Use the ArduinoValueReader script to get joystick input
+        float joystickX = float.Parse(ArduinoValueReader.Instance.value);
+
+        // Adjust the cursor movement based on joystick input
+        moveDirection.x += joystickX;
+
+        // Move the cursor
+        transform.Translate(moveDirection * cursorSpeed * Time.deltaTime);
+    }
+}
