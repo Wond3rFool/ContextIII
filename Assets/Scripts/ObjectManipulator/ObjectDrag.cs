8d52ea45ee61d818965eea6f0022b879d1b5ecfc
Tiago V <T.Verstoep96@gmail.com>
2023-12-13 14:38:49 +0100

    Drag object from anywhere
    hey

--- Assets/Scripts/ObjectManipulator/ObjectDrag.cs
+++ Assets/Scripts/ObjectManipulator/ObjectDrag.cs
@@ -7,47 +7,39 @@ public class ObjectDrag : MonoBehaviour
     private Camera cam;
 
     private bool isDragging;
-
-    private float rotationSpeed = 90.0f;
-
-    private Renderer objectRenderer;
-
-    private Material objectMaterial1;
-
-    private HandleButton handleButton;
+    private Vector3 offset;
 
     private void Start()
     {
         isDragging = false;
         cam = FindAnyObjectByType<Camera>();
-        handleButton = FindAnyObjectByType<HandleButton>();
-        objectRenderer = GetComponentInChildren<Renderer>();
     }
-
 
     private void Update()
     {
-        Vector3 forward = Camera.main.transform.forward;
-        forward.y = 0; // Make sure the rotation is only in the horizontal plane
-        forward.Normalize();
-        string[] values = handleButton.GetValue();
-
         if (isDragging)
         {
-            // Needed to move the object around with mouse position.
-            transform.position = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 7));
+            // Calculate the new position based on the offset
+            Vector3 newPosition = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 7)) + offset;
+
+            // Set the new position
+            transform.position = newPosition;
         }
-
-
     }
 
     private void OnMouseDown()
     {
+        // Calculate the offset between the object's position and the mouse position
+        offset = transform.position - cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 7));
+
+        // Start dragging
         isDragging = true;
     }
 
     private void OnMouseUp()
     {
+        // Stop dragging
         isDragging = false;
     }
+
 }
